2210acbe2d196ff5c30ab4dd41bbe1f20eba9ddf
Scriptales <scriptales.dev@gmail.com>
2019-12-05 12:07:45 +0100

    Ajout des méthodes correspondant aux fonctionnalités définies

--- models/Annonce.php
+++ models/Annonce.php
@@ -4,6 +4,32 @@ namespace Models;
 
 class Annonce {
 
+    public function selectAll() {
 
+    }
+
+    public function select() {
+
+    }
+
+    public function selectByCat() {
+
+    }
+
+    public function selectByUser() {
+
+    }
+
+    public function insert() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
 
 }
--- models/Utilisateur.php
+++ models/Utilisateur.php
@@ -4,6 +4,28 @@ namespace Models;
 
 class Utilisateur {
 
+    public function selectAll() {
 
+    }
+
+    public function select() {
+
+    }
+
+    public function verifByMail() {
+        
+    }
+
+    public function insert() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+        
+    }
 
 }
